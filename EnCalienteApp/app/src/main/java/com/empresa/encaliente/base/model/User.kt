2fe3637dc0bb9a0089627e56f1b9fdab5232a2ac
norman.sanchez <normansanchezn@gmail.com>
2020-10-18 21:58:39 -0500

    [ADD] Flujo hasta términos y condiciones (solo views)

---
+++ EnCalienteApp/app/src/main/java/com/empresa/encaliente/base/model/User.kt
@@ -0,0 +1,11 @@
+package com.empresa.encaliente.base.model
+
+/**
+ * Created by Norman Sánchez on 18/10/20.
+ */
+data class User (
+    val name: String = "",
+    val lastName: String = "",
+    val email: String = "",
+    val phoneNumber: String = ""
+)
